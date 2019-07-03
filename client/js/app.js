3187067eeef1ebfaf2597a75b408c20e1609255f
Tiago <tiagofabre@gmail.com>
2019-07-02 23:13:17 -0300

    server with tcp and udp setup

--- client/js/app.js
+++ client/js/app.js
@@ -620,3 +620,21 @@ function resize() {
 
     socket.emit('windowResized', { screenWidth: global.screenWidth, screenHeight: global.screenHeight });
 }
+
+var PORT = 33333;
+var HOST = '127.0.0.1';
+
+var dgram = require('dgram');
+var message = new Buffer('My KungFu is Good!');
+
+var client = dgram.createSocket('udp4');
+
+setInterval(sendMsg, 300)
+
+function sendMsg() {
+    client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+    if (err) throw err;
+    console.log('UDP message sent to ' + HOST +':'+ PORT);
+    
+    });
+}
